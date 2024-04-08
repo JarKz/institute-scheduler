1e6feb5ca60d3d84d85ccf171693562b43868e9f
Pavel <work.belyavsky.p.a@gmail.com>
2024-04-08 09:09:11 +0300

    feat: added java spring backend

---
+++ backend/src/main/java/jarkz/institutescheduler/InstituteSchedulerApplication.java
@@ -0,0 +1,13 @@
+package jarkz.institutescheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstituteSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstituteSchedulerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/jarkz/institutescheduler/InstituteSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package jarkz.institutescheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstituteSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
