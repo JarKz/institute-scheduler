5f6be278f2cb9369cc3edd88974c3622a742d07f
Pavel <work.belyavsky.p.a@gmail.com>
2024-04-11 10:22:53 +0300

    refactor: formatted code in main file

--- backend/src/main/java/jarkz/institutescheduler/InstituteSchedulerApplication.java
+++ backend/src/main/java/jarkz/institutescheduler/InstituteSchedulerApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InstituteSchedulerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InstituteSchedulerApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(InstituteSchedulerApplication.class, args);
+  }
 }
