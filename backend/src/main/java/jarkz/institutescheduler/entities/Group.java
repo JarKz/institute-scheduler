0f30e9cfac5e19021e8b97189d8fae1ecf4103d9
Pavel <work.belyavsky.p.a@gmail.com>
2024-04-25 14:24:19 +0300

    feat: added Group entity

---
+++ backend/src/main/java/jarkz/institutescheduler/entities/Group.java
@@ -0,0 +1,26 @@
+package jarkz.institutescheduler.entities;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import lombok.ToString;
+
+@Entity
+@ToString
+public class Group {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  public String name;
+
+  @ManyToMany(cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
+  public List<Student> students;
+}
