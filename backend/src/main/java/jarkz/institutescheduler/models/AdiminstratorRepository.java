9017bb4a9d3d80cd2ac3fe8ed59f97360e587e49
Pavel <work.belyavsky.p.a@gmail.com>
2024-04-25 13:34:00 +0300

    fix: swapped generics in JPA repo-interface

--- backend/src/main/java/jarkz/institutescheduler/models/AdiminstratorRepository.java
+++ backend/src/main/java/jarkz/institutescheduler/models/AdiminstratorRepository.java
@@ -3,5 +3,5 @@ package jarkz.institutescheduler.models;
 import jarkz.institutescheduler.entities.Administrator;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AdiminstratorRepository extends JpaRepository<Long, Administrator> {}
+public interface AdiminstratorRepository extends JpaRepository<Administrator, Long> {}
 
