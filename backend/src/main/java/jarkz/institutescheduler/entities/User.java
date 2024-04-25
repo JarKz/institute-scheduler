3f4ac1d3b58e135d55971ced588c09daa7d305d6
Pavel <work.belyavsky.p.a@gmail.com>
2024-04-25 13:34:32 +0300

    fix: added misssing annotation for enabling fields

--- backend/src/main/java/jarkz/institutescheduler/entities/User.java
+++ backend/src/main/java/jarkz/institutescheduler/entities/User.java
@@ -5,6 +5,9 @@ import java.util.Collection;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class User implements UserDetails {
   
   public String username;
