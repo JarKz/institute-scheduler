01096aefbd4575fdd3ffe27a8df6d6d95697170f
Pavel <work.belyavsky.p.a@gmail.com>
2024-04-30 15:48:18 +0300

    feat: added login controller

---
+++ src/main/java/jarkz/institutescheduler/controllers/Login.java
@@ -0,0 +1,21 @@
+package jarkz.institutescheduler.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/login")
+public class Login {
+
+  @GetMapping
+  public String getLoginPage(
+      @RequestParam(name = "error", required = false) String errorMessage, Model model) {
+    if (errorMessage != null) {
+      model.addAttribute("error_message", errorMessage);
+    }
+    return "sign/login";
+  }
+}
