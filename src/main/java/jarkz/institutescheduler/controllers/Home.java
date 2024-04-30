bb9fb63e79ffc2104dacb0a04694c0edbe2ed088
Pavel <work.belyavsky.p.a@gmail.com>
2024-04-30 18:13:57 +0300

    refactor: removed redundant annotation

--- src/main/java/jarkz/institutescheduler/controllers/Home.java
+++ src/main/java/jarkz/institutescheduler/controllers/Home.java
@@ -2,10 +2,8 @@ package jarkz.institutescheduler.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
 public class Home {
 
   @GetMapping("/home")
