0964d9de15474277b531610708643a5876f66473
Pavel <work.belyavsky.p.a@gmail.com>
2024-04-30 18:40:08 +0300

    fix: removed @EnableWebMvc to enable static files

--- src/main/java/jarkz/institutescheduler/configs/WebConfig.java
+++ src/main/java/jarkz/institutescheduler/configs/WebConfig.java
@@ -2,11 +2,9 @@ package jarkz.institutescheduler.configs;
 
 import jarkz.institutescheduler.controllers.Views;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@EnableWebMvc
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
