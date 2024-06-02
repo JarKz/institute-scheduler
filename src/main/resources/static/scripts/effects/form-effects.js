58a1a220f5951ba5dfc92b461848507fc2cae2f9
Pavel <work.belyavsky.p.a@gmail.com>
2024-06-02 23:15:24 +0300

    feat: added login html page with css and js

---
+++ src/main/resources/static/scripts/effects/form-effects.js
@@ -0,0 +1,37 @@
+function bindEffects(inputElement) {
+  inputElement.addEventListener("focusin", (event) => {
+    const effectElement = document.createElement("span");
+    let rectangle = event.currentTarget.getBoundingClientRect();
+    let rectangleFormPage = event.currentTarget.parentNode.getBoundingClientRect();
+    effectElement.style.top = `${rectangle.top - rectangleFormPage.top}px`;
+    effectElement.style.left = `${rectangle.left - rectangleFormPage.left}px`;
+    event.currentTarget.parentNode.insertBefore(effectElement, event.currentTarget.nextSibling);
+    effectElement.classList = ["show-color-outline"];
+  }, false);
+
+  inputElement.addEventListener("focusout", () => {
+    let allEffectElements = document.getElementsByClassName("show-color-outline");
+    for (let element of allEffectElements) {
+      element.classList = ["erase-color-outline"];
+      setTimeout(() => {
+        element.remove();
+      }, 400);
+    }
+  }, false);
+}
+
+const inputElements = document.getElementsByTagName("input");
+
+for (const inputElement of inputElements) {
+  bindEffects(inputElement);
+}
+
+const errorMessage = document.getElementsByClassName("error");
+
+if (errorMessage.length !== 0) {
+  const inputElementsForSetBorderColor = document.getElementsByTagName("input");
+  for (const element of inputElementsForSetBorderColor) {
+    element.style.borderColor = "rgba(255, 0, 0, 0.9)";
+  }
+}
+
