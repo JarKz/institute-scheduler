77e3890b8a31e72f1e7dbf7911b1522a353ca705
Pavel <work.belyavsky.p.a@gmail.com>
2024-06-18 01:28:17 +0300

    feat: added business logic for calendar

---
+++ src/main/java/jarkz/institutescheduler/businesslogic/BusinessLogicService.java
@@ -0,0 +1,19 @@
+package jarkz.institutescheduler.businesslogic;
+
+import jarkz.institutescheduler.models.ScheduleRepository;
+import jarkz.institutescheduler.models.StudentRepository;
+import jarkz.institutescheduler.models.TeacherRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BusinessLogicService {
+
+  @Bean
+  public Events getEvents(
+      StudentRepository studentRepository,
+      TeacherRepository teacherRepository,
+      ScheduleRepository scheduleRepository) {
+    return new Events(studentRepository, teacherRepository, scheduleRepository);
+  }
+}
---
+++ src/main/java/jarkz/institutescheduler/businesslogic/Events.java
@@ -0,0 +1,39 @@
+package jarkz.institutescheduler.businesslogic;
+
+import jarkz.institutescheduler.entities.Schedule;
+import jarkz.institutescheduler.models.ScheduleRepository;
+import jarkz.institutescheduler.models.StudentRepository;
+import jarkz.institutescheduler.models.TeacherRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Events {
+
+  private StudentRepository studentRepository;
+  private TeacherRepository teacherRepository;
+  private ScheduleRepository scheduleRepository;
+
+  public Events(
+      StudentRepository studentRepository,
+      TeacherRepository teacherRepository,
+      ScheduleRepository scheduleRepository) {
+    this.studentRepository = studentRepository;
+    this.teacherRepository = teacherRepository;
+    this.scheduleRepository = scheduleRepository;
+  }
+
+  public List<Schedule> getUpcomingShedulesFor(final String username) {
+    var student = studentRepository.findByUsername(username);
+    if (student != null) {
+      return scheduleRepository.findByStudentsContainingAndLessonDateGreaterThanEqual(student, LocalDate.now());
+    }
+
+    var teacher = teacherRepository.findByUsername(username);
+    if (teacher != null) {
+      return scheduleRepository.findByTeacherAndLessonDateGreaterThanEqual(teacher, LocalDate.now());
+    }
+
+    return List.of();
+  }
+}
