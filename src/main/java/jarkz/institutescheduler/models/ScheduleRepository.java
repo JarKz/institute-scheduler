8ae9842bb7996420099610e469dbcbc6901d4865
Pavel <work.belyavsky.p.a@gmail.com>
2024-06-18 01:27:31 +0300

    feat: updated schedule repo for upcoming events

--- src/main/java/jarkz/institutescheduler/models/ScheduleRepository.java
+++ src/main/java/jarkz/institutescheduler/models/ScheduleRepository.java
@@ -1,7 +1,15 @@
 package jarkz.institutescheduler.models;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import jarkz.institutescheduler.entities.Schedule;
+import jarkz.institutescheduler.entities.Student;
+import jarkz.institutescheduler.entities.Teacher;
 
-public interface ScheduleRepository extends JpaRepository<Schedule, Long> {}
+public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+  List<Schedule> findByStudentsContainingAndLessonDateGreaterThanEqual(Student student, LocalDate lessonDate);
+  List<Schedule> findByTeacherAndLessonDateGreaterThanEqual(Teacher teacher, LocalDate lessonDate);
+}
